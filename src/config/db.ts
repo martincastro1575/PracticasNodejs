c94ae4d35f17267c80742d04707d3aa0722c6204
MartinC <64977451+martincastro1575@users.noreply.github.com>
2025-02-12 15:27:07 -0300

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,13 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
 
     try {
-        const url = 'mongodb+srv://root:BybxsnrFPQciUrD7@cluster0.ctg6a.mongodb.net/linktree_node_typescript'
-        const { connection } = await mongoose.connect(url)
 
-        const url2 = `${connection.host}:${connection.port}`
-        
-        
-        console.log(`MongoDB Conectado en: ${url2}`);
         
     } catch (error) {
         console.log(error.message);
